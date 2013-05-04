0dfa1ef92894598720fd735de8062f0515725081
siguremon <siguremon@gmail.com>
2013-05-05 02:24:17 +0900

    add function to add events from calendar

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -1,5 +1,24 @@
 $(document).ready(function() {
-    $('#calendar').fullCalendar({
-	events: '/events.json'
-    })
+    var select = function(start, end, allDay) {
+	var title = window.prompt("title");
+	var data = {event: {title: title,
+			    start: start,
+			    end: end, 
+			    allDay: allDay}};
+	$.ajax({
+	    type: "POST",
+	    url: "../events",
+	    data: data,
+	    success: function() {
+		calendar.fullCalendar('refetchEvents');
+	    }
+	});
+    };
+
+    var calendar = $('#calendar').fullCalendar({
+	events: '/events.json',
+	selectable: true,
+	ignoreTimezone: false,
+	select: select
+    });
 });
