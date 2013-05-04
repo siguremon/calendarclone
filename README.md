bad7f1fc1413d537bd8a91b8801805a11fc2c840
siguremon <siguremon@gmail.com>
2013-05-05 03:40:55 +0900

    add debug code

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Ruby on Railsとfullcalendar pluginで作るgoogleカレンダークローン
 
 http://qiita.com/items/73651af19babd22fe012
+
+http://qiita.com/items/10216d15471dfe5ba572
--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
     var select = function(start, end, allDay) {
+	console.log('start:' + start);
+	console.log('end:' + end);
+	console.log('allDay:' + allDay);
 	var title = window.prompt("title");
 	var data = {event: {title: title,
 			    start: start,
@@ -7,17 +10,19 @@ $(document).ready(function() {
 			    allDay: allDay}};
 	$.ajax({
 	    type: "POST",
-	    url: "../events",
+	    url: "/events",
 	    data: data,
 	    success: function() {
 		calendar.fullCalendar('refetchEvents');
 	    }
 	});
+	calendar.fullCalendar('unselect');
     };
 
     var calendar = $('#calendar').fullCalendar({
 	events: '/events.json',
 	selectable: true,
+	selectHelper: true,
 	ignoreTimezone: false,
 	select: select
     });
