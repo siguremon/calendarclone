e929a940968997bb0e654c67598ba8ebd0caf370
siguremon <siguremon@gmail.com>
2013-05-06 08:56:35 +0900

    add callback eventClick

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -19,11 +19,30 @@ $(document).ready(function() {
 	calendar.fullCalendar('unselect');
     };
 
+    var eventClick = function(event, jsEvent, view) {
+	var title = window.prompt("title", event.title);
+	var url = "/events/" + event.id;
+	var data = {_method: 'PUT',
+		    event: {title: title,
+			    start: event.start,
+			    end: event.end, 
+			    allDay: event.allDay}};
+	$.ajax({
+	    type: "POST",
+	    url: url,
+	    data: data,
+	    success: function() {
+		calendar.fullCalendar('refetchEvents');
+	    }
+	});
+    };
+
     var calendar = $('#calendar').fullCalendar({
 	events: '/events.json',
 	selectable: true,
 	selectHelper: true,
 	ignoreTimezone: false,
-	select: select
+	select: select,
+	eventClick: eventClick
     });
 });
